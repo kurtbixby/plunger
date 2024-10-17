21d8366d0762fc4cf61760b716398c76191ffeac
Kurt Bixby <kurtbixby@gmail.com>
2024-10-17 18:11:12 -0500

    Added migrations for updated Cover model

---
+++ Plunger.Data/Migrations/20241017224251_UpdatedCoverModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Plunger.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedCoverModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Covers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Covers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
